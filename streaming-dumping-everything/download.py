1972a56858c2e8c49759f36a7ffcd6b1d5637d60
Pikatsuto <pikatsuto@gmail.com>
2024-12-07 19:50:46 +0100

    feat: skip useless download and auto replace VO by VF

--- streaming-dumping-everything/download.py
+++ streaming-dumping-everything/download.py
@@ -19,7 +19,29 @@ class Download:
             except Exception:
                 continue
 
-    def download_video(self, url, path):
+    def download_video(self, url, path, folder, lang):
+        videos_path_with_extentions = [
+            path + ".mp4",
+            path + ".webm"
+        ]
+
+        for video_path in videos_path_with_extentions:
+            print(video_path)
+            if os.path.exists(video_path):
+                return
+
+        name = path.split("/")[-1].split(" ")
+        name.pop(-1)
+        name = " ".join(name)
+        for i in os.listdir(folder):
+            old_lang: str = i.split(".")[0].split(" ")[-1]
+            old_name = i.split(" ")
+            old_name.pop(-1)
+            old_name = " ".join(old_name)
+            if old_lang != "VF" and lang == "VF" and old_name == name:
+                print(f"delete VO from VF : {path}")
+                os.remove(folder + i)
+
         ydl_opts = {
             'format':
                 'webm[height=1080],'
@@ -39,7 +61,9 @@ class Download:
                     os.system(f"mkdir -p './data/{episode['path']}'")
                     self.download_video(
                         lecteur,
-                        f"./data/{episode['fullPath']}"
+                        f"./data/{episode['fullPath']}",
+                        f"./data/{episode['path']}",
+                        episode['lang'],
                     )
                     break
                 except Exception:
